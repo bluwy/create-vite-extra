fa2ff02dbc659ca9f71406e114b26b1e9b074287
bluwy <bjornlu.dev@gmail.com>
2022-07-25 16:10:09 +0800

    feat: setup react

--- template-react/src/entry-client.jsx
+++ template-react/src/entry-client.jsx
@@ -1,9 +1,10 @@
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.hydrateRoot(
+  document.getElementById('root'),
   <React.StrictMode>
     <App />
   </React.StrictMode>
