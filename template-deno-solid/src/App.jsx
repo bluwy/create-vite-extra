c75a35d9d9af8c736c61e1a1afb2eb0702a7d958
bluwy <bjornlu.dev@gmail.com>
2022-10-17 22:56:23 +0800

    Update existing template styles

--- template-deno-solid/src/App.jsx
+++ template-deno-solid/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + Solid</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count()}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
