d54e9ed702efc8ce18389106a2d9ef4b0c1225a8
bluwy <bjornlu.dev@gmail.com>
2022-11-15 14:08:49 +0800

    fix: update version import path

--- template-deno-react/vite.config.mjs
+++ template-deno-react/vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'npm:vite@^3.2.3'
 import react from 'npm:@vitejs/plugin-react@^2.2.0'
 
 import 'npm:react@^18.2.0'
-import 'npm:react-dom/client@^18.2.0'
+import 'npm:react-dom@^18.2.0/client'
 
 // https://vitejs.dev/config/
 export default defineConfig({
