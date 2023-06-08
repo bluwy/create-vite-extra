f643e6ec7ce3e94596e9f216a3ca2286f0463d7f
bluwy <bjornlu.dev@gmail.com>
2023-06-08 17:31:33 +0800

    Backport https://github.com/vitejs/vite/commit/3679bd757f1b24d40f7c66ace14167b97f8093cc

--- template-deno-react/src/App.jsx
+++ template-deno-react/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <>
       <img src="/vite-deno.svg" alt="Vite with Deno" />
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </div>
+    </>
   )
 }
 
--- template-ssr-react-ts/src/App.tsx
+++ template-ssr-react-ts/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -27,7 +27,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </div>
+    </>
   )
 }
 
