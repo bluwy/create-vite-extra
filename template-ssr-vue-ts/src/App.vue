c49701a2d674ad1ac6355f61413f20ffeba46f34
pratik149 <pratikrane149@gmail.com>
2023-12-27 23:50:09 +0530

    Update title in `template-ssr-vue-ts`

--- template-ssr-vue-ts/src/App.vue
+++ template-ssr-vue-ts/src/App.vue
@@ -13,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue + TS" />
 </template>
 
 <style scoped>
