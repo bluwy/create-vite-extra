a7563da646ce06f3bb03e09275cd39301ebcba55
Muhammad <phonzammizaki@gmail.com>
2024-01-13 10:07:25 +0700

    add template-ssr-vue-streaming-ts

--- template-ssr-vue-streaming-ts/server.js
+++ template-ssr-vue-streaming-ts/server.js
@@ -46,7 +46,7 @@ app.use('*', async (req, res) => {
       // Always read fresh template in development
       template = await fs.readFile('./index.html', 'utf-8')
       template = await vite.transformIndexHtml(url, template)
-      render = (await vite.ssrLoadModule('/src/entry-server.js')).render
+      render = (await vite.ssrLoadModule('/src/entry-server.ts')).render
     } else {
       template = templateHtml
       render = (await import('./dist/server/entry-server.js')).render
