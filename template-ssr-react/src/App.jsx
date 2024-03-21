d0553c6f336dfa663b6e4aa0d0815eed01336bf4
Ivan Badicean <badican01117@gmail.com>
2024-03-21 11:46:09 +0200

    Use `renderToPipeableStream` for react

--- template-ssr-react/src/App.jsx
+++ template-ssr-react/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { Suspense, lazy } from "react"
+import reactLogo from "./assets/react.svg"
+import "./App.css";
+
+// Works also with SSR as expected
+const Content = lazy(() => import('./Content'));
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -15,20 +16,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Suspense fallback={<p>Loading content</p>}>
+        <Content />
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- template-ssr-react/src/entry-server.jsx
+++ template-ssr-react/src/entry-server.jsx
@@ -1,12 +1,43 @@
 import React from 'react'
-import ReactDOMServer from 'react-dom/server'
+import { renderToPipeableStream } from 'react-dom/server'
 import App from './App'
 
-export function render() {
-  const html = ReactDOMServer.renderToString(
+const ABORT_DELAY = 10000;
+
+export function render(res, template) {
+  let didError = false;
+  
+  const { pipe, abort } = renderToPipeableStream(
     <React.StrictMode>
       <App />
-    </React.StrictMode>
-  )
-  return { html }
+    </React.StrictMode>,
+    {
+      onShellError() {
+        res.sendStatus(500);
+      },
+      onAllReady() {
+        res.status(didError ? 500 : 200);
+        res.set({ 'Content-Type': 'text/html' });
+
+        const [first, second] = template.split(`<!--app-html-->`);
+
+        // Can use react-helmet-async here
+        const head = first.replace(`<!--app-head-->`, '<title>Vite + React SSR</title>');
+
+        res.write(head);
+        pipe(res);
+        res.write(second);
+
+        res.end();
+      },
+      onError(error) {
+        didError = true;
+        console.error(error);
+      }
+    },
+  );
+
+  setTimeout(() => {
+    abort();
+  }, ABORT_DELAY);
 }
