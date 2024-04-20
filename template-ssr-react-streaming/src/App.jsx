52b9c8320804794ccd37e0b1509e6719b2ea7bf9
Ivan Badicean <badican01117@gmail.com>
2024-04-20 19:54:08 +0300

    Added lazy load of `Card` component

--- template-ssr-react-streaming/src/App.jsx
+++ template-ssr-react-streaming/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Suspense, lazy } from "react"
+import reactLogo from "./assets/react.svg"
+import "./App.css"
+
+// Works also with SSR as expected
+const Card = lazy(() => import("./Card"))
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -16,14 +17,11 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+
+      <Suspense fallback={<p>Loading card component...</p>}>
+        <Card />
+      </Suspense>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
