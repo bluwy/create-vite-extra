70c051c6efcd84c4147809761d8026c5db495250
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2025-02-21 22:36:53 +0900

    Remove jsx deno directive comments

--- template-deno-react/src/main.jsx
+++ template-deno-react/src/main.jsx
@@ -1,7 +1,5 @@
 import './index.css'
-// @deno-types="@types/react"
 import { StrictMode } from 'react'
-// @deno-types="@types/react-dom/client"
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
