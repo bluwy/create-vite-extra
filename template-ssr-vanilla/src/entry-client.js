524ace7820be188b86a92a4ec801d33465dbaa30
bluwy <bjornlu.dev@gmail.com>
2026-09-09 09:58:15 +0800

    Allow ssr-vanilla client build to detect imported assets
    close #91

--- template-ssr-vanilla/src/entry-client.js
+++ template-ssr-vanilla/src/entry-client.js
@@ -1,4 +1,5 @@
 import './style.css'
+import './app' // Ensure imported assets are included in the client bundle
 import { setupCounter } from './counter'
 
 setupCounter(document.querySelector('#counter'))
